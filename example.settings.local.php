0381d31022586bfcfad0eff87a70c1f47e8f40c0
Gabriel Carneiro <biel.dev@outlook.com>
2026-04-08 10:45:46 -0300

    fix: replace cache.backend.null with cache.backend.memory

--- example.settings.local.php
+++ example.settings.local.php
@@ -40,9 +40,9 @@ $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess']  = FALSE;
 
 // Cache nulo em dev evita ter que rodar "drush cr" toda hora.
-$settings['cache']['bins']['render']             = 'cache.backend.null';
-$settings['cache']['bins']['page']               = 'cache.backend.null';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['render']             = 'cache.backend.memory';
+$settings['cache']['bins']['page']               = 'cache.backend.memory';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.memory';
 
 // Erros visíveis no browser, em produção isso fica em 'hide'.
 $config['system.logging']['error_level'] = 'verbose';
